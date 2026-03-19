5656be89bdb26395652ecbe26546a38521a2de77
Quy Nguyen <quynguyen96.dev@gmail.com>
2026-03-19 11:11:35 +0700

    Add root url

--- DemoCICD/Program.cs
+++ DemoCICD/Program.cs
@@ -21,4 +21,6 @@ app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
 });
 
+app.MapGet("/", () => "Welcome to demo CI CD in K8S");
+
 app.Run();
